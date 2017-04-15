4c35c0316c279c392d23e10289a1b6e837d95407
N8Stewart <Nate.Stewart12@gmail.com>
2017-04-14 23:34:26 -0700

    Initial commit of Maven

---
+++ src/main/java/com/stewart/Main.java
@@ -0,0 +1,12 @@
+package com.stewart;
+
+/**
+ * Hello world main using Maven
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
