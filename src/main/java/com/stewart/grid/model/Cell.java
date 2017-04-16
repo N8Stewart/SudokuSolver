64e45a4a944b5b9da48b68c58ffdee069198cc82
N8Stewart <Nate.Stewart12@gmail.com>
2017-04-16 12:50:03 -0700

    Rename tentatives to possibilities. Add clearPossibility method. Minor refactoring. Tests added and old tests formatted.

--- src/main/java/com/stewart/grid/model/Cell.java
+++ src/main/java/com/stewart/grid/model/Cell.java
@@ -8,14 +8,14 @@ import java.util.Arrays;
 public class Cell {
 
     private int value;
-    private int[] tentative; // if tentative[1] == 1, then value can be 1.
+    private int[] possibility; // if tentative[1] == 1, then value can be 1.
 
     /**
      * Create a blank cell with an entropy of 9
      */
     public Cell() {
         this.value = 0;
-        this.tentative = new int[]{0, 1, 1, 1, 1, 1, 1, 1, 1, 1}; // offset by 1
+        this.possibility = new int[]{0, 1, 1, 1, 1, 1, 1, 1, 1, 1}; // offset by 1
     }
 
     /**
@@ -24,13 +24,10 @@ public class Cell {
      * @param value The value of the cell
      */
     public Cell(final int value) {
-        validate(value);
-        this.value = value;
-        this.tentative = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // offset by 1
-        this.tentative[value] = 1;
+        setValue(value);
     }
 
-    private void validate(int value) {
+    private void validate(final int value) {
         if (value <= 0 || value > 9)
             throw new IllegalArgumentException(String.format("Invalid Cell value '%d'. Value must be an integer between 1-9.", value));
     }
@@ -42,6 +39,8 @@ public class Cell {
     public void setValue(final int value) {
         validate(value);
         this.value = value;
+        this.possibility = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // offset by 1
+        this.possibility[value] = 1;
     }
 
     /**
@@ -52,7 +51,26 @@ public class Cell {
      * @return an integer from 0 to 9. An entropy of 0 means there is no value for the cell which solves the puzzle.
      */
     public int getEntropy() {
-        return Arrays.stream(this.tentative).sum();
+        return Arrays.stream(this.possibility).sum();
+    }
+
+    /**
+     * Clear a possible value from the list of possibilities.
+     * If the possibility of {@code value} is 1, decrease the entropy by 1.
+     *
+     * @param value the value to clear from the list of possibilities
+     * @return the value of this cell if entropy == 1; otherwise, return 0
+     */
+    public int clearPossibility(final int value) {
+        validate(value);
+        this.possibility[value] = 0;
+        this.value = 0;
+        if (getEntropy() == 1)
+            for (int i = 1; i <= 9; i++)
+                if (this.possibility[i] == 1)
+                    this.value = i;
+
+        return this.value;
     }
 
 }
--- src/test/java/com/stewart/grid/model/CellTest.java
+++ src/test/java/com/stewart/grid/model/CellTest.java
@@ -27,25 +27,26 @@ public class CellTest {
         try {
             new Cell(-1);
             fail("Cell value of -1 is not a valid cell value.");
-        } catch(IllegalArgumentException ignored) {}
+        } catch (IllegalArgumentException ignored) {
+        }
     }
-
 
     @Test
     public void testInstantiationFailsForInvalidCellValue0() {
         try {
             new Cell(0);
             fail("Cell value of 0 is not a valid cell value.");
-        } catch(IllegalArgumentException ignored) {}
+        } catch (IllegalArgumentException ignored) {
+        }
     }
-
 
     @Test
     public void testInstantiationFailsForInvalidCellValue10() {
         try {
             new Cell(10);
             fail("Cell value of 10 is not a valid cell value.");
-        } catch(IllegalArgumentException ignored) {}
+        } catch (IllegalArgumentException ignored) {
+        }
     }
 
     @Test
@@ -54,9 +55,9 @@ public class CellTest {
             final Cell cell = new Cell();
             cell.setValue(-1);
             fail("Cell value of -1 is not a valid cell value.");
-        } catch(IllegalArgumentException ignored) {}
+        } catch (IllegalArgumentException ignored) {
+        }
     }
-
 
     @Test
     public void testSetValueFailsForInvalidCellValue0() {
@@ -64,9 +65,9 @@ public class CellTest {
             final Cell cell = new Cell();
             cell.setValue(0);
             fail("Cell value of 0 is not a valid cell value.");
-        } catch(IllegalArgumentException ignored) {}
+        } catch (IllegalArgumentException ignored) {
+        }
     }
-
 
     @Test
     public void testSetValueFailsForInvalidCellValue10() {
@@ -74,7 +75,65 @@ public class CellTest {
             final Cell cell = new Cell();
             cell.setValue(10);
             fail("Cell value of 10 is not a valid cell value.");
-        } catch(IllegalArgumentException ignored) {}
+        } catch (IllegalArgumentException ignored) {
+        }
+    }
+
+    @Test
+    public void testClearPossibilityFailsForInvalidCellValueNegative1() {
+        try {
+            final Cell cell = new Cell();
+            cell.clearPossibility(-1);
+            fail("Cell value of -1 is not a valid cell value.");
+        } catch (IllegalArgumentException ignored) {
+        }
+    }
+
+    @Test
+    public void testClearPossibilityForInvalidCellValue0() {
+        try {
+            final Cell cell = new Cell();
+            cell.clearPossibility(0);
+            fail("Cell value of 0 is not a valid cell value.");
+        } catch (IllegalArgumentException ignored) {
+        }
+    }
+
+    @Test
+    public void testClearPossibilityFailsForInvalidCellValue10() {
+        try {
+            final Cell cell = new Cell();
+            cell.clearPossibility(10);
+            fail("Cell value of 10 is not a valid cell value.");
+        } catch (IllegalArgumentException ignored) {
+        }
+    }
+
+    @Test
+    public void testClearPossibilitiesToCheckCorrectValuesAreAssigned() {
+        final Cell cell = new Cell();
+        for (int i = 1; i <= 9; i++) {
+            assertEquals(10 - i, cell.getEntropy());
+            cell.clearPossibility(i);
+            assertEquals(9 - i, cell.getEntropy());
+            if (i != 8) {
+                assertEquals(0, cell.getValue());
+            } else {
+                assertEquals(9, cell.getValue());
+            }
+        }
+    }
+
+    @Test
+    public void testSetValueSetsCorrectValueAndResetsEntropy() {
+        final Cell cell = new Cell();
+        assertEquals(9, cell.getEntropy());
+        assertEquals(0, cell.getValue());
+
+        cell.setValue(1);
+
+        assertEquals(1, cell.getEntropy());
+        assertEquals(1, cell.getValue());
     }
 
 }
