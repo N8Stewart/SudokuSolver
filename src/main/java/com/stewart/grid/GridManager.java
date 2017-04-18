71e3b4ea9a2af9f9bef68827ad771c18b4f82648
N8Stewart <Nate.Stewart12@gmail.com>
2017-04-17 19:50:54 -0700

    Create empty stub for a grid management class.

---
+++ src/main/java/com/stewart/grid/GridManager.java
@@ -0,0 +1,12 @@
+package com.stewart.grid;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * A concrete class to control the interactions with a grid
+ */
+@ToString
+@EqualsAndHashCode
+public class GridManager {
+}
